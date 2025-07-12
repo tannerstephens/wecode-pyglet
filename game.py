4e9fed8954e3b67877d296d71662bf5175b56d50
Tanner Stephens <tanner@tannerstephens.com>
2025-07-12 09:40:40 -0500

    WARNING

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
-"""THIS IS REQUIRED FOR PYGLET TO FUNCTION ON THE PI5"""
+"""THE BELOW IS REQUIRED FOR PYGLET TO FUNCTION ON THE PI5"""
 
 import os
 
 os.environ["LIBGL_ALWAYS_SOFTWARE"] = "1"
+
+"""THE ABOVE IS REQUIRED FOR PYGLET TO FUNCTION ON THE PI5"""
 
 from pyglet.shapes import Rectangle
 
@@ -13,7 +15,9 @@ BACKGROUND_COLOR = (255, 255, 255)
 
 class Game(BaseGame):
     def load_resources(self):
-        self.background = Rectangle(0, 0, self.width, self.height, color=BACKGROUND_COLOR)
+        self.background = Rectangle(
+            0, 0, self.width, self.height, color=BACKGROUND_COLOR
+        )
 
     def on_draw(self):
         self.clear()
